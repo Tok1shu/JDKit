1d37f1aab9a98129ef785a21a23c0f149b42ddd9
Tokishu <arch.wizard.megumi@gmail.com>
2026-03-01 01:07:17 +0100

    Maven Central publishing setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,56 @@ plugins {
     kotlin("plugin.spring") version "2.2.21"
     `java-library`
     `maven-publish`
+    signing
 }
 
 group = "dev.tokishu"
 version = "1.0"
+
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "dev.tokishu"
+            artifactId = "jdkit"
+            version = "1.0.0"
+
+            pom {
+                name.set("JDKit")
+                description.set(" A lightweight, scalable, and Spring-integrated Discord bot framework built on top of JDA. ")
+                url.set("https://github.com/Tok1shu/JDKit")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://mit-license.org/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("P30wo9")
+                        name.set("Tokishu")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Tok1shu/JDKit.git")
+                    developerConnection.set("scm:git:ssh://github.com/Tok1shu/JDKit.git")
+                    url.set("https://github.com/Tok1shu/JDKit")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // Используйте URL из письма от Sonatype (обычно s01 для новых аккаунтов)
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.findProperty("ossrhUsername")?.toString()
+                password = project.findProperty("ossrhPassword")?.toString()
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -20,22 +66,27 @@ dependencies {
     // Spring Boot Integration (Optional)
     compileOnly("org.springframework.boot:spring-boot-autoconfigure:3.2.3")
     compileOnly("org.springframework:spring-context:6.1.4")
-    
+
     // Configuration YAML (Jackson)
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
-    
+
     // Dotenv
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.8.0")
-    
+
     // Logging Logging facade and default implementation
     api("org.slf4j:slf4j-api:2.0.12")
     implementation("ch.qos.logback:logback-classic:1.5.3")
     implementation("org.codehaus.janino:janino:3.1.12")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 kotlin {
@@ -50,13 +101,6 @@ tasks.register<JavaExec>("runApp") {
     classpath = sourceSets["main"].runtimeClasspath
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            groupId = "dev.tokishu"
-            artifactId = "jdkit"
-            version = "1.0.0"
-        }
-    }
+signing {
+    sign(publishing.publications["mavenJava"])
 }
